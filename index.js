b86a8b36d7007610fb7d18afea9c9e759ff15b8e
yosmoc <yoshihisa.mochihara@gmail.com>
2016-06-15 23:07:47 +0200

    implement npm start to open the main window

---
+++ index.js
@@ -0,0 +1,35 @@
+const electron = require('electron');
+const {app} = electron;
+const {BrowserWindow} = electron;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  win.loadURL('http://www.ehonnavi.net/');
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
